a985cd7a8a8f2a33c0c15bca7b9cdd46a4b745ef
sahoodhirendra30 <63303181+sahoodhirendra30@users.noreply.github.com>
2021-05-02 23:48:59 +0200

    Update PaymentApplicationTests.java

--- Payment/src/test/java/com/dhirendra/PaymentApplicationTests.java
+++ Payment/src/test/java/com/dhirendra/PaymentApplicationTests.java
@@ -1,6 +1,10 @@
 package com.dhirendra;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -27,6 +31,9 @@ class PaymentApplicationTests {
 
 	@LocalServerPort
 	int randomServerPort;
+	
+	@Autowired
+	PaymentService paymentService;
 
 	private static final String BASE_URL = "http://localhost:";
 
@@ -87,6 +94,52 @@ class PaymentApplicationTests {
 
 		// Verify request succeed
 		assertThat(result.getStatusCodeValue()).isEqualTo(422);
+	}
+	
+	@Test
+	public void testPaymentResponse() throws URISyntaxException, JsonProcessingException {
+
+		Resource requestBody = new ClassPathResource("input.json", this.getClass());
+		Resource responseBody = new ClassPathResource("output.json", this.getClass());
+
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setInterceptors(Collections.singletonList(new ContentInterceptor(requestBody)));
+		MockRestServiceServer mockServer = MockRestServiceServer.bindTo(restTemplate).build();
+
+		HttpHeaders headers = new HttpHeaders();
+
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+		map.add("xRequestId", "29318e25-cebd-498c-888a-f77672f66449");
+		map.add("signatureCertificate", "lox3QmEaxEGU");
+		map.add("signature", "lox3QmEaxEGU");
+		headers.addAll(map);
+
+		mockServer.expect(requestTo(new URI("/v1.0.0/initiate-payment")))
+				.andExpect(method(HttpMethod.POST))
+				.andRespond(withSuccess(responseBody, MediaType.APPLICATION_JSON).headers(headers));
+		
+		restTemplate.postForObject("/v1.0.0/initiate-payment", requestBody, PaymentResponse.class);
+
+	}
+
+	private static class ContentInterceptor implements ClientHttpRequestInterceptor {
+
+		private final Resource resource;
+
+		private ContentInterceptor(Resource resource) {
+			this.resource = resource;
+		}
+
+		@Override
+		public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
+				throws IOException {
+			ClientHttpResponse response = execution.execute(request, body);
+			byte[] expected = FileCopyUtils.copyToByteArray(this.resource.getInputStream());
+			byte[] actual = FileCopyUtils.copyToByteArray(response.getBody());
+			assertEquals(new String(expected), new String(actual));
+			return response;
+
+		}
 	}
 
 	
